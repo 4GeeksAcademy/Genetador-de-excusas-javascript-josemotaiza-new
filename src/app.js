483cc07377f3a65ff253d3ea5e95bdc5fab803ea
josemotaiza <145352234+josemotaiza@users.noreply.github.com>
2024-03-12 14:16:30 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+const obtenerElementoRandom = listaElementos => {
+  let elemento = " ";
+  elemento = listaElementos[Math.floor(Math.random() * listaElementos.length)];
+  console.log(elemento);
+  return elemento;
+};
+const onLoad = () => {
+  let generarExcusa = document.querySelector("#excuse");
+  generarExcusa.innerHTML =
+    obtenerElementoRandom(who) +
+    " " +
+    obtenerElementoRandom(action) +
+    " " +
+    obtenerElementoRandom(what) +
+    " " +
+    obtenerElementoRandom(when);
+  console.log(generarExcusa);
+};
+onLoad();
